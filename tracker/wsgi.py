e8d83a4b3e7d2c6eba87d83d3e796c8d5417feb7
Michaela <michaela.arteberry@gmail.com>
2024-02-05 15:13:27 -0500

    Feature 2 complete

---
+++ tracker/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for tracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+import django
+django.setup()
+
+from django.core.management import call_command
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings")
+
+application = get_wsgi_application()
